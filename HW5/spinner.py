edd2d063c69e69fbc5425d07a47741d287b40b40
Rtvik Sriram Bharadwaj <rbharadwaj71@gmail.com>
2019-10-10 15:57:08 -0400

    finishing spinner

--- HW5/spinner.py
+++ HW5/spinner.py
@@ -3,6 +3,8 @@ from machine import RTC, Timer, Pin, ADC, PWM, I2C
 from math import atan, sqrt
 import ubinascii
 
+old_temp = 0
+
 def convert(val):
     if val & (1 << 15):
         val = val - 1 << 16
@@ -17,23 +19,25 @@ def interface_sensors():
         i2c.writeto_mem(83, 0x1F, b'\x00')
         i2c.writeto_mem(83, 0x20, b'\x00')
         i2c.writeto_mem(83, 0x31, b"\b00001000")
-    else:
         print("yeet")
         
     if i2c.readfrom_mem(72, 0x0B, 1) == b'\xcb':
         i2c.writeto_mem(72, 0x03, b'\x80')
-    else:
-        print("temp sensor not recognized")
+        print("temp sensor recognized")
     
 # 3.2.3 Demo
 def spinner_demo(p):
+    global old_temp
     ax = convert(int.from_bytes(i2c.readfrom_mem(83, 0x33, 1) + i2c.readfrom_mem(83, 0x32, 1), "big"))/25
     ay = convert(int.from_bytes(i2c.readfrom_mem(83, 0x35, 1) + i2c.readfrom_mem(83, 0x34, 1), "big"))/25
-    az = convert(int.from_bytes(i2c.readfrom_mem(83, 0x37, 1) + i2c.readfrom_mem(83, 0x36, 1), "big"))/(25 * 9.8)
+    az = convert(int.from_bytes(i2c.readfrom_mem(83, 0x37, 1) + i2c.readfrom_mem(83, 0x36, 1), "big"))/(25)
     if ax >= -0.5 and ax <= 0.5 and ay >= -0.5 and ay <= 0.5:
         green_led.on()
     else:
         green_led.off()
+        
+    if ax >= 10 or ax <= -10 or ay >= 10 or ay <= -10 or az >= 10 or az <= -10:
+        red_led.on()
     print("XYZ =", ax, ay, az)
     pitch = atan(ax/sqrt(ay*2 + az*2)) * 57.2958
     roll  = atan(ay/sqrt(ax*2 + az*2)) * 57.2958
@@ -49,24 +53,31 @@ def spinner_demo(p):
     data = bytearray(2)
     i2c.readfrom_mem_into(72, 0x00, data)
     value = data[0] << 8 | data[1]
-    temp = (value & 0b111111111111) / 125
+    temp = (value & 0b111111111111) / 128
+    if old_temp == 0:
+        old_temp = temp
+    pwm_cycle = PWM(Pin(13), freq=(10 - int(temp - old_temp)), duty=512)
     print("Temperature =", temp)
     print("--------------------------------------------")
 
 def button_irqhandler_1(p):
-    red_led = Pin(15, Pin.OUT)
-    red_led.on()
+    onboard_led = Pin(13, Pin.OUT)
+    onboard_led.on()
+    green_led.off()
+    yellow_led.off()
     interface_sensors()
     
 def button_irqhandler_2(p):
     pwm_cycle = PWM(Pin(13), freq=10, duty=512)
-    tim1 = Timer(0)
     tim1.init(period=1000, mode=Timer.PERIODIC, callback=spinner_demo)
 
+# Initialize timers
+tim1 = Timer(0)
+
 # Initialize leds
-red_led = Pin(15, Pin.OUT)
 green_led = Pin(32, Pin.OUT)
 yellow_led = Pin(33, Pin.OUT)
+red_led = Pin(14, Pin.OUT)
 
 # Initialize buttons
 button1 = Pin(21, Pin.IN, Pin.PULL_DOWN)
@@ -79,6 +90,6 @@ button2.irq(trigger=Pin.IRQ_RISING, handler=button_irqhandler_2)
 i2c = I2C(scl=Pin(22, Pin.PULL_UP), sda=Pin(23, Pin.PULL_UP), freq=400000)
 
 # comment this out during demo
-# print(i2c.scan())
-# button_irqhandler_1(1)
-# button_irqhandler_2(1)
+green_led.off()
+yellow_led.off()
+red_led.off()
