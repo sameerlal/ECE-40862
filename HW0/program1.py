8786dbcdefe382b25f2997a0c17452de4c8490e2
Sameer Lal <sjl328@cornell.edu>
2022-02-05 21:42:22 -0500

    Make year more generic
    yo need a favor, can you message me?

--- HW0/program1.py
+++ HW0/program1.py
@@ -1,6 +1,9 @@
 # RTVIK SRIRAM BHARADWAJ
 # bharadwr[AT]purdue[DOT]edu
+from datetime import datetime
+year = datetime.now().year
+
 name = input('What is your name? ')
 age = input('How old are you? ')
-year = 100 - int(age) + 2019
-print(name + " will be 100 years old in the year " + str(year))
+year = 100 - int(age) + year
+print(f"{name} will be 100 years old in the year {year}")
